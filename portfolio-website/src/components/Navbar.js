4c38d8ab7d237d98e87123c74c4fbad73264d2c8
George Nassar <35306293+gnassar97@users.noreply.github.com>
2021-12-19 15:18:14 -0700

    Moved away from inline styling

--- portfolio-website/src/components/Navbar.js
+++ portfolio-website/src/components/Navbar.js
@@ -2,33 +2,45 @@ import React from "react";
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import { LinkContainer } from "react-router-bootstrap";
+import './Navbar.css';
 
 
 function MainNavbar(){
     return (
         <Navbar bg="light" expand="lg" sticky="top">
                 <Container>
-                    <LinkContainer to="/home" style={{ textDecoration: 'none' }}>
-                        <Navbar.Brand href="#home">George Nassar</Navbar.Brand>
-                    </LinkContainer>
+                    <div className="Navbar-linkitem">
+                        <LinkContainer to="/home">
+                            <Navbar.Brand href="#home">George Nassar</Navbar.Brand>
+                        </LinkContainer>
+                    </div>
+                    
                     
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <LinkContainer to="/home" style={{ textDecoration: 'none' }}>
+                        <div className="Navbar-linkitem">
+                            <LinkContainer to="/home">
                                 <Nav.Link>Home</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/projects" style={{ textDecoration: 'none' }}>
+                        </div>
+                        <div className="Navbar-linkitem">
+                            <LinkContainer to="/projects">
                                 <Nav.Link>Projects</Nav.Link>
                             </LinkContainer>
-                            <div style={{marginLeft: '300%'}}>
-                                <LinkContainer to="/about" style={{ textDecoration: 'none' }}>
+                        </div>
+                            <div className="Navbar-about">
+                                <div className="Navbar-linkitem">
+                                <LinkContainer to="/about">
                                     <Nav.Link>About</Nav.Link>
                                 </LinkContainer>
+                                </div>
                             </div>
-                                <LinkContainer to="/contact" style={{ textDecoration: 'none' }}>
+                            <div className="Navbar-linkitem">
+                                <LinkContainer to="/contact">
                                     <Nav.Link>Contact</Nav.Link>
                                 </LinkContainer>
+                            </div>
                             
                             
                         </Nav>
